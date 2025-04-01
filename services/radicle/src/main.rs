f9432ce49792ceac46fbc080d6ba320bd4b8af25
Neil Herriford <neil.herriford@gmail.com>
2025-04-01 01:18:55 -0400

    porting radicle to rust

---
+++ services/radicle/src/main.rs
@@ -0,0 +1,68 @@
+use clap::{Arg, Command};
+use docker::images;
+
+// mod services;
+// use services::action::Action;
+// use services::docker::images::list::List;
+
+#[tokio::main]
+async fn main() {
+    let cmd_bootstrap: &str = "bootstrap";
+    // let arg_server: &str = "server";
+
+    let matches = Command::new("radicle")
+        .version("0.01")
+        .author("Neil Herriford")
+        .about("Bootstrap for douglas")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+        .subcommand(
+            Command::new(cmd_bootstrap)
+                .about("Bootstrap douglas")
+                .arg(
+                    Arg::new("start")
+                        .long("start")
+                        .help("Starts the bootstrap proccess")
+                        .action(clap::ArgAction::SetTrue),
+                )
+                .arg_required_else_help(true),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some((cmd_bootstrap, sub_matches)) => {
+            if sub_matches.get_flag("start") {
+                let body = images::list().await.unwrap();
+                println!("{:?}", body);
+                // let l = List;
+                // let _ = l.fart().await;
+            }
+        }
+        _ => todo!(),
+    }
+
+    // .arg(
+    //     Arg::new(ARG_BOOTSTRAP)
+    //         .long(ARG_BOOTSTRAP)
+    //         .help("Starts the bootstrap proccess")
+    //         .action(clap::ArgAction::SetTrue)
+    //         .conflicts_with(arg_server),
+    // )
+    // .arg(
+    //     Arg::new(arg_server)
+    //         .long(ARG_SERVER)
+    //         .help("Starts the admin server")
+    //         .action(clap::ArgAction::SetTrue)
+    //         .conflicts_with(ARG_BOOTSTRAP),
+    // )
+    // .get_matches();
+
+    // if matches.get_flag("fart") {
+    //     println!("--fart flag is set!");
+    //     let l = List;
+    //     let _ = l.fart().await;
+    // } else {
+    //     println!("--fart flag is not set.");
+    // }
+}
