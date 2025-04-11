a1b9fa9fc25f68689a07dbbd1b50bb11a151e175
Neil Herriford <neil.herriford@gmail.com>
2025-04-10 23:51:23 -0400

    split off io!

---
+++ services/radicle/unix_domain_socket_rest_client/src/io_builder.rs
@@ -0,0 +1,20 @@
+use hyper::rt::{Read, Write};
+use std::error::Error;
+use std::future::Future;
+use std::pin::Pin;
+
+pub(crate) trait IoStream: Read + Write + Unpin + Send {}
+
+impl<T: Read + Write + Unpin + Send> IoStream for T {}
+
+pub(crate) trait IoBuilder {
+    fn build<'a>(
+        &'a self,
+    ) -> Pin<
+        Box<
+            dyn Future<Output = Result<Box<dyn IoStream>, Box<dyn Error + Send + Sync>>>
+                + Send
+                + 'a,
+        >,
+    >;
+}
---
+++ services/radicle/unix_domain_socket_rest_client/src/unix_domain_socket_io_builder.rs
@@ -0,0 +1,34 @@
+use crate::io_builder::{IoBuilder, IoStream};
+use hyper_util::rt::TokioIo;
+use std::error::Error;
+use std::future::Future;
+use std::path::Path;
+use std::pin::Pin;
+use tokio::net::UnixStream;
+
+pub(crate) struct UnixDomainSocketIoBuilder {
+    socket_file_path: String,
+}
+
+impl UnixDomainSocketIoBuilder {
+    pub fn new(socket_file_path: String) -> Self {
+        Self { socket_file_path }
+    }
+}
+
+impl IoBuilder for UnixDomainSocketIoBuilder {
+    fn build<'a>(
+        &'a self,
+    ) -> Pin<
+        Box<
+            dyn Future<Output = Result<Box<dyn IoStream>, Box<dyn Error + Send + Sync>>>
+                + Send
+                + 'a,
+        >,
+    > {
+        Box::pin(async move {
+            let stream = UnixStream::connect(Path::new(&self.socket_file_path)).await?;
+            Ok(Box::new(TokioIo::new(stream)) as Box<dyn IoStream>)
+        })
+    }
+}
